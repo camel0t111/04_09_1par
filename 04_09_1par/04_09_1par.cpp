258beda9212f077d1b1e101ce4626980d70a033e
Kadik_px66@student.itstep.org <Kadik_px66@student.itstep.org>
2024-09-04 09:40:39 +0300

    Добавлена функция 'Sum' - выводит сумму 2ч чисел

--- 04_09_1par/04_09_1par.cpp
+++ 04_09_1par/04_09_1par.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+void Sum(int a, int b)
+{
+	cout << a + b << endl;
+}
+
 int main()
 {
 	
@@ -15,18 +20,18 @@ int main()
 			
 			
 			
-							
-							
-							
-							
-			
+											
+											
+											
+											
+											
 			
 			
 			
 			
 
 
-
+	
 
 
 
