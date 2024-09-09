31165f7f48d3dc827d7a45fdc79b71814ece500b
camel0t111 <109875158+camel0t111@users.noreply.github.com>
2024-09-09 08:57:55 +0300

    Update 04_09_1par.cpp

--- 04_09_1par/04_09_1par.cpp
+++ 04_09_1par/04_09_1par.cpp
@@ -1,52 +1,71 @@
 #include <iostream>
 using namespace std;
 
-void Sum(int a, int b)
+class Fraction
 {
-	cout << a + b << endl;
-}
+private:
+    int x;
+    int y;
+public:
+    Fraction(int valueX, int valueY)
+    {
+        x = valueX;
+        y = valueY;
+    }
+    void SetX(int valueX)
+    {
+        cout << "Введите X: ";
+        cin >> valueX;
+        x = valueX;
+    }
+    void SetY(int valueY)
+    {
+        cout << "Введите Y: ";
+        cin >> valueY;
+        y = valueY;
+    }
+    void Calc(int choice)
+    {
+        cout << "Выберите желаемую операцию:\n";
+        cout << "1. Сложение\n";
+        cout << "2. Вычитание\n";
+        cout << "3. Умножение\n";
+        cout << "4. Деление\n";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "X" << "+" << "Y"<< "=" << x + y;
+            break;
+        case 2:
+            cout << "X" << "-" << "Y"<< "=" << x - y;
+            break;
+        case 3:
+            cout << "X" << "*" << "Y" << "=" << x * y;
+            break;
+        case 4:
+            cout << "X" << "/" << "Y" << "=" << x / y;
+            break;
+        default:
+            cout << "Неизвестная операция.";
+            break;
+        }
+    }
+    void Print()
+    {
+        cout << "X = " << x << "\t Y = " << y << endl;
+    }
+};
+
+
 
 int main()
 {
-	cout << "program menu" << endl;
+    setlocale(LC_ALL, "ru");
 
-
-
-
-
-
-
-			
-			
-			
-			
-											
-											
-											
-											
-											
-			
-			
-			
-			
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
+    Fraction a(0, 0);
+    a.SetX(0);
+    a.SetY(0);
+    a.Print();
+    a.Calc(0);
 }
-
